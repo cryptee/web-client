d9121d4bb0a1eb39e0a1a810a30600410d893547
John Ozbay <info@crypt.ee>
2018-06-26 14:13:15 +0300

    Bug Fixes & Updates 🐞🎉
    + Moves homedoc.json out of docs.js, reducing docs.js weight by ~95kilograms. 🏋🏻‍♂️
    + Updated Embedded Image Attachment Modal
    + Fixes a bug which caused tags (#) to trigger in “codeblocks” making typing comments for some languages an experience straight from hell.

--- source/js/main.js
+++ source/js/main.js
@@ -501,6 +501,20 @@ function deleteAllCookies() {
 deleteAllCookies();
 
 
+// SHORTCUT FOR AN AJAX JSON LOADER
+
+function loadJSON(url, callback) {
+
+   var xobj = new XMLHttpRequest();
+   xobj.overrideMimeType("application/json");
+   xobj.open('GET', url, true); url
+   xobj.onreadystatechange = function () {
+     if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+     }
+   };
+   xobj.send(null);
+}
 
 
 ////////////////////////////////////////////////////
